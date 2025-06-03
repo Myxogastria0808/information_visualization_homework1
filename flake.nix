f8407f9a1fb442ce0c01bd13b34ff43f4f8cd140
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-06-03 14:53:44 +0900

    initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "r flake sample";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+        rpkgs = with inputs.nixpkgs.legacyPackages.${system}.rPackages; [
+          ggplot2
+          ggsci
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ R quarto ] ++ rpkgs;
+        };
+      }
+    );
+}
